82898256b46b6190970dbdfc18ae38cdcc254cf6
Lightning Huang <lihuang@microsoft.com>
2016-09-19 17:57:44 +0800

    small tuning

---
+++ AlterRankerEnsembleWeights/AlterRankerEnsembleWeights/IniParser.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AlterRankerEnsembleWeights
+{
+    public class IniParser
+    {
+        private Hashtable keyPairs = new Hashtable();
+        private string iniFilePath;
+
+        private struct SectionPair
+        {
+            public string Section;
+            public string Key;
+        }
+
+        public IniParser(string iniPath)
+        {
+            TextReader iniFile = null;
+            string strLine = null;
+            string currentRoot = null;
+            string[] keyPair = null;
+            iniFilePath = iniPath;
+
+            if (File.Exists(iniPath))
+            {
+                try
+                {
+                    iniFile = new StreamReader(iniPath);
+                    strLine = iniFile.ReadLine();
+                    while (strLine != null)
+                    {
+                        strLine = strLine.Trim();
+                        if (strLine.StartsWith(";"))
+                        {
+                            strLine = iniFile.ReadLine();
+                            continue;
+                        }
+                        if (strLine != "")
+                        {
+                            if (strLine.StartsWith("[") && strLine.EndsWith("]"))
+                            {
+                                // all the sectionName is in lowercase except ROOT
+                                currentRoot = strLine.Substring(1, strLine.Length - 2).ToLower();
+                            }
+                            else
+                            {
+                                string multilineItem = strLine;
+                                while (multilineItem != null && multilineItem.EndsWith("_"))
+                                {
+                                    multilineItem = iniFile.ReadLine();
+                                    strLine = strLine.Remove(strLine.Length - 1, 1) + multilineItem;
+                                }
+                                keyPair = strLine.Split(new char[] { '=' }, 2);
+                                SectionPair sectionPair;
+                                string value = null;
+                                if (currentRoot == null)
+                                    currentRoot = "root";
+                                sectionPair.Section = currentRoot;
+                                sectionPair.Key = keyPair[0];
+                                if (keyPair.Length > 1)
+                                    value = keyPair[1];
+                                keyPairs[sectionPair] = value;
+                            }
+                        }
+                        strLine = iniFile.ReadLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(iniPath);
+                    Console.WriteLine(currentRoot);
+                    Console.WriteLine(keyPair[0]);
+                    Console.WriteLine(keyPair[1]);
+                    throw ex;
+                }
+                finally
+                {
+                    if (iniFile != null)
+                        iniFile.Close();
+                }
+            }
+            else
+                throw new FileNotFoundException("Unable to locate " + iniPath);
+
+        }
+
+        public string GetSetting(string sectionName, string settingName)
+        {
+            SectionPair sectionPair;
+            sectionPair.Section = sectionName.ToLower();
+            sectionPair.Key = settingName;
+
+            return (string)keyPairs[sectionPair];
+        }
+
+
+
+        public string[] EnumSection(string sectionName)
+        {
+            List<string> tmpArray = new List<string>();
+            foreach (SectionPair pair in keyPairs.Keys)
+            {
+                if (pair.Section == sectionName.ToLower())
+                    tmpArray.Add(pair.Key);
+            }
+            return tmpArray.ToArray();
+        }
+    }
+}
---
+++ AlterRankerEnsembleWeights/AlterRankerEnsembleWeights/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AlterRankerEnsembleWeights
+{
+    class WeightsAndBias
+    {
+        public double[] weights;
+        public double bias;
+    }
+    class Program
+    {
+        private static string MakeENumberString(int count)
+        {
+            List<string> es = new List<string>();
+            for (int i = 1; i <= count; ++i)
+            {
+                es.Add("E:" + i);
+            }
+            return string.Join("\t", es);
+        }
+
+        private static WeightsAndBias GetWeightsAndBias(string weightsTxt)
+        {
+            WeightsAndBias result = new WeightsAndBias();
+            using (var reader = new StreamReader(weightsTxt))
+            {
+                string buf = null;
+                List<double> weights = new List<double>();
+                double bias = 0.0;
+                int counter = 1;
+                while ((buf = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(buf))
+                    {
+                        continue;
+                    }
+                    if (buf.StartsWith("E"))
+                    {
+                        var tokens = buf.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        // fill the gap between full and this actual results
+                        for (; ("E" + counter) != tokens[0]; counter++)
+                        {
+                            weights.Add(0.0);
+                        }
+                        if (tokens.Length == 2)
+                        {
+                            weights.Add(double.Parse(tokens[1]));
+                        }
+                        else
+                        {
+                            weights.Add(double.Parse(tokens[1]));
+                            buf = reader.ReadLine();
+                            if (string.IsNullOrWhiteSpace(buf))
+                            {
+                                throw new Exception("no bias found!");
+                            }
+                            bias = double.Parse(buf);
+                        }
+                        counter++;
+                    }
+                }
+                result.weights = weights.ToArray();
+                result.bias = bias;
+
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var weightsTxt = args[0];
+            var modelIni = args[1];
+            var output = args[2];
+            var parser = new IniParser(modelIni);
+
+            var evaluatorsString = parser.GetSetting("TreeEnsemble", "Evaluators");
+            if (evaluatorsString == null)
+            {
+                throw new Exception("the model file is not a valid TreeEnsemble format.");
+            }
+            var result = GetWeightsAndBias(weightsTxt);
+            var evaluators = int.Parse(evaluatorsString);
+            var aggregatorIndex = -1;
+            if (string.Equals(parser.GetSetting("Evaluator:" + evaluators, "EvaluatorType"), "Aggregator", StringComparison.OrdinalIgnoreCase))
+            {
+                if (string.Equals(parser.GetSetting("Evaluator:" + (evaluators - 1), "EvaluatorType"), "Aggregator", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (parser.GetSetting("Evaluator:" + (evaluators - 1), "Weights") != null)
+                    {
+                        aggregatorIndex = evaluators - 1;
+                    }
+                    else
+                    {
+                        throw new Exception("it seems the last 2nd was the weights, but did not find it.");
+                    }
+                }
+                else
+                {
+                    if (parser.GetSetting("Evaluator:" + evaluators, "Weights") != null)
+                    {
+                        aggregatorIndex = evaluators;
+                    }
+                    else
+                    {
+                        throw new Exception("it seems the last was the weights, but did not find it.");
+                    }
+                }
+            }
+            // aggregatorIndex==-1, add one section
+            // aggregatorIndex==other, just change that one
+            bool hitAggregator = false;
+            bool completeWriteWeights = false;
+            bool completeWriteBias = false;
+            using (var reader = new StreamReader(modelIni))
+            {
+                using (var writer = new StreamWriter(output))
+                {
+                    string buf = null;
+                    while ((buf = reader.ReadLine()) != null)
+                    {
+                        if (buf.StartsWith("Evaluators"))
+                        {
+                            if (aggregatorIndex == -1)
+                            {
+                                writer.WriteLine("Evaluators=" + (evaluators + 1));
+                                continue;
+                            }
+                        }
+                        else if (buf.Contains("[Evaluator:" + aggregatorIndex + "]"))
+                        {
+                            hitAggregator = true;
+                        }
+                        else if (buf.StartsWith("Weights") && hitAggregator && !completeWriteWeights && aggregatorIndex != -1)
+                        {
+                            writer.WriteLine("Weights=" + string.Join("\t", result.weights.Select(item => item.ToString("0.######"))));
+                            completeWriteWeights = true;
+                            continue;
+                        }
+                        else if (buf.StartsWith("Bias") && hitAggregator && !completeWriteBias && aggregatorIndex != -1)
+                        {
+                            writer.WriteLine("Bias=" + result.bias.ToString("0.######"));
+                            completeWriteBias = true;
+                            continue;
+                        }
+                        writer.WriteLine(buf);
+                    }
+                    if (aggregatorIndex == -1)
+                    {
+                        writer.WriteLine();
+                        writer.WriteLine("[Evaluator:" + (evaluators + 1) + "]");
+                        writer.WriteLine("EvaluatorType=Aggregator");
+                        writer.WriteLine("NumNodes=" + evaluators);
+                        writer.WriteLine("Nodes=" + MakeENumberString(evaluators));
+                        writer.WriteLine("Weights=" + string.Join("\t", result.weights.Select(item => item.ToString("0.######"))));
+                        writer.WriteLine("Type=Linear");
+                        writer.WriteLine("Bias=" + result.bias.ToString("0.######"));
+                    }
+                }
+            }
+        }
+    }
+}
